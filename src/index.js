04114b34a9f7e7d581cb33ca283c2180c7a35298
denisdelaki <denisdelaki@gmail.com>
2022-06-13 21:52:05 +0300

    finished creating the to do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector("#create-task-form");
+  form.addEventListener('submit', function (e){
+    e.preventDefault()
+    todoHandler(e.target.newTaskDescription.value)
+      todoHandler(e.target.day.value);
+    
+  })
+  
+  function todoHandler(newTodo) {
+    let p = document.createElement("p");
+    let delet = document.createElement('button');
+   delet.textContent =  'x'
+    delet.addEventListener('click', deleteHandler)
+    p.textContent = `${newTodo} `;
+    p.appendChild(delet);
+    document.querySelector("#tasks").appendChild(p);
+  }
+  function deleteHandler(e) {
+    e.target.parentNode.remove()
+  }
+  // p.push(date);
+  // let date = { checked: false, id: Date.now() };
 });
+ 
